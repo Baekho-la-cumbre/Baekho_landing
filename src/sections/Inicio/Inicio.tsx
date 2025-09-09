b983ed6030aa10fdffe472f76f4f9ef633fd9ea1
DLeonardoG <darkleonardog@gmail.com>
2025-09-08 22:08:32 -0500

    style: :lipstick: Styling start

--- src/sections/Inicio/Inicio.tsx
+++ src/sections/Inicio/Inicio.tsx
@@ -36,16 +36,16 @@ export default function Inicio(): JSX.Element {
               hideInicio ? "opacity-0 pointer-events-none" : "opacity-100"
             } relative`}
           >
-            <h2 className="text-3xl font-extrabold text-white md:text-4xl drop-shadow-lg">
+            <h2 className="font-extrabold text-white text-1xl md:text-4xl drop-shadow-lg">
               CLUB DEPORTIVO
             </h2>
-            <h1 className="mb-5 text-transparent mfont-extrabold text-8xl md:text-9xl bg-gradient-to-r from-orange-500 to-red-600 bg-clip-text drop-shadow-lg baekho-title">
+            <h1 className="mb-5 text-6xl text-transparent mfont-extrabold md:text-9xl bg-gradient-to-r from-orange-500 to-red-600 bg-clip-text drop-shadow-lg baekho-title">
               BAEKHO
             </h1>
-            <p className="text-lg text-gray-200 md:text-xl">
+            <p className="text-sm text-gray-200 md:text-xl">
               Forjando campeones, inspirando futuros.
             </p>
-            <p className="mb-8 text-lg font-semibold text-red-400 md:text-xl">
+            <p className="mb-8 text-sm font-semibold text-red-400 md:text-xl">
               Descubre la excelencia en Taekwondo.
             </p>
           </div>
